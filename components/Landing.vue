0910500015e39c3f018948a52ce7ddb72af4fba9
irawanyodi <dykhazta@gmail.com>
2025-05-26 03:27:22 +0700

    adjust landing page

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,31 +1,35 @@
 <template>
   <section ref="landingRef"
-    class="relative flex items-end md:items-center justify-center h-screen bg-no-repeat bg-cover bg-[center_80%] md:bg-[center_35%]"
+    class="relative flex justify-center h-screen bg-no-repeat bg-cover bg-[center_80%] md:bg-[center_35%]"
     style="background-image: url('/images/landing.webp')">
     <!-- Overlay -->
-    <div class="absolute inset-0 bg-gradient-to-b from-black/60 to-black/30 z-0"></div>
+    <div class="absolute inset-0 bg-gradient-to-b from-black/10 to-red-950/90 z-0"></div>
 
     <!-- Content -->
-    <div class="relative mb-16 text-white text-center px-8 py-10 rounded-3xl shadow-2xl max-w-lg animate-fade-in">
-      <p class="font-second text-xl">The Wedding Of</p>
-      <h1 class="text-5xl font-heading font-bold tracking-wide my-8">
-        Desti & Reza
-      </h1>
-      <p class="text-sm md:text-lg font-second font-light text-white/90 italic">
-        Kepada Yth:
-      </p>
-      <p class="font-second text-lg">{{ guestName }}</p>
-      <button @click="openInvitation"
-        class="mt-4 inline-flex items-center gap-2 px-6 py-2 bg-white text-gray-800 font-second font-semibold rounded-3xl shadow-md hover:bg-gray-100 transition">
-        <span>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-            <path fill-rule="evenodd"
-              d="M2.106 6.447A2 2 0 0 0 1 8.237V16a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V8.236a2 2 0 0 0-1.106-1.789l-7-3.5a2 2 0 0 0-1.788 0l-7 3.5Zm1.48 4.007a.75.75 0 0 0-.671 1.342l5.855 2.928a2.75 2.75 0 0 0 2.46 0l5.852-2.927a.75.75 0 1 0-.67-1.341l-5.853 2.926a1.25 1.25 0 0 1-1.118 0l-5.856-2.928Z"
-              clip-rule="evenodd" />
-          </svg>
-        </span>
-        <span>Buka Undangan</span>
-      </button>
+    <div class="relative grid place-content-between text-white text-center rounded-3xl max-w-lg animate-fade-in py-20">
+      <div>
+        <p class="font-second text-xl">The Wedding Of</p>
+        <h1 class="text-5xl font-heading font-bold tracking-wide mt-5">
+          Desti & Reza
+        </h1>
+      </div>
+      <div>
+        <p class="text-sm md:text-lg font-second font-light text-white/90">
+          Kepada Yth:
+        </p>
+        <p class="font-second font-semibold text-lg">{{ guestName }}</p>
+        <button @click="openInvitation"
+          class="mt-4 inline-flex items-center gap-2 px-6 py-2 bg-white text-gray-800 font-second font-semibold rounded-3xl shadow-md hover:bg-gray-100 transition">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+              <path fill-rule="evenodd"
+                d="M2.106 6.447A2 2 0 0 0 1 8.237V16a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V8.236a2 2 0 0 0-1.106-1.789l-7-3.5a2 2 0 0 0-1.788 0l-7 3.5Zm1.48 4.007a.75.75 0 0 0-.671 1.342l5.855 2.928a2.75 2.75 0 0 0 2.46 0l5.852-2.927a.75.75 0 1 0-.67-1.341l-5.853 2.926a1.25 1.25 0 0 1-1.118 0l-5.856-2.928Z"
+                clip-rule="evenodd" />
+            </svg>
+          </span>
+          <span>Buka Undangan</span>
+        </button>
+      </div>
     </div>
   </section>
 </template>
@@ -53,7 +57,7 @@ const openInvitation = () => {
   setTimeout(() => {
     emit('open')
     window.dispatchEvent(new Event('play-music'))
-  }, 700)
+  }, 100)
 }
 </script>
 
