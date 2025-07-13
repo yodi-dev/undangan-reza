28c1d75c501fd471f5f21486deb8cbda431361fe
irawanyodi <dykhazta@gmail.com>
2025-07-13 17:41:45 +0700

    update og

--- layouts/defaul.vue
+++ layouts/defaul.vue
@@ -3,3 +3,16 @@
     <NuxtPage />
   </div>
 </template>
+<script setup>
+useHead({
+  title: 'Desti & Reza Wedding - awan-dev.site',
+  meta: [
+    { name: 'description', content: 'Kami mengundang Anda untuk hadir di hari bahagia kami.' },
+    { property: 'og:title', content: 'Desti & Reza Wedding - awan-dev.site' },
+    { property: 'og:description', content: 'Kami mengundang Anda untuk hadir di hari bahagia kami.' },
+    { property: 'og:image', content: 'https://undangan-reza.vercel.app/images/1.webp' },
+    { property: 'og:url', content: 'https://undangan-reza.vercel.app' },
+    { property: 'og:type', content: 'website' },
+  ]
+})
+</script>
